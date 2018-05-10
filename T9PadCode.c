8de8d948a9f03e64ff84ba1af569a404edd71aa3
gowtham1996 <gowthamspikes@gmail.com>
2018-05-11 03:21:33 +0530

    Create T9PadCode.c

---
+++ T9PadCode.c
@@ -0,0 +1,30 @@
+// Sample code to perform I/O:
+#include <stdio.h>
+
+int main(){
+	int num;char a[50][1000];int i;
+	scanf("%d", &num);              			// Reading input from STDIN
+	printf("Input number is %d.\n", num);       // Writing output to STDOUT
+
+
+for(i=0;i<num;i++)
+{
+    scanf("%s",a[i][1000]);
+}
+char b[10][4]={{'.',',','?','!'},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'}
+,{'p','q','r','s'},{'t','u','v'},
+{'w','x','y','z'},{'0'}};
+
+
+for(i=0;i<num;i++)
+{
+    for(j=0;j<1000;j++)
+    
+}}
+
+void GetCharr(char m)
+{
+    for(i=0;i<10;i++)
+      for(j=0;j<4;j++)
+      
+}
