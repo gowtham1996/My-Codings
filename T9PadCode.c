d49a26effd42066475486ffba4a0c37874a545af
gowtham1996 <gowthamspikes@gmail.com>
2018-05-15 22:18:19 +0530

    Update T9PadCode.c

--- T9PadCode.c
+++ T9PadCode.c
@@ -5,21 +5,17 @@
 Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
-
 #include <stdio.h>
+int num,i;char a[50][1000],PreviousCount,CurrentCount,count;
+int j;
+char b[10][4]={{'.',',','?','!'},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'}
+,{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'},{'0'}};
 
+void GetCharr(char);
 int main()
 {
-	int num,i;char a[50][1000];
-	int j;
-	char b[10][4]={{'.',',','?','!'},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'}
-,{'p','q','r','s'},{'t','u','v'},
-{'w','x','y','z'},{'0'}};
 printf("Enter the no. of characters you want to enter");
-	scanf("%d", &num);              			// Reading input from STDIN
-	printf("Input number is %d.\n", num);       // Writing output to STDOUT
-
-
+scanf("%d", &num);          
 for(i=0;i<num;i++)
 {
     scanf("%s",a[i]);
@@ -28,9 +24,10 @@ for(i=0;i<num;i++)
 printf("%c",a[1][3]);
 
 for(i=0;i<num;i++)
-{
-        for(j=0;a[i][j]!=' ';j++)
-        printf("\nLength of the String : %d",j);
+{count=0;
+    for(j=0;a[i][j]!='\0';j++)
+       GetCharr(a[i][j]);
+       printf("\n Time Take %d",count);
 }
     
 }
@@ -38,6 +35,22 @@ for(i=0;i<num;i++)
 void GetCharr(char m)
 {
     for(int i=0;i<10;i++)
-      for(int j=0;j<4;j++);
+      for(int j=0;j<4;j++)
+          if(b[i][j]==m)
+          {
+              if(count!=0)
+              {
+                  if(PreviousCount!=CurrentCount)
+                  {
+                      count++;
+                  }
+              }
+              else
+              {
+                  count++;
+                  PreviousCount=i;
+                  CurrentCount=i;
+              }
+          }
       
 }
